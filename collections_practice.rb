773ef37d9c2c3d003a533970a486137adf6cf86f
Keith Isham <kisham8899@gmail.com>
2019-04-28 17:36:55 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,33 @@
+def sort_array_asc(sort)
 
+sort.sort
+end
+
+def sort_array_desc(a)
+  a.sort.reverse
+end
+
+def sort_array_char_count(a)
+
+  a = a.sort_by(&:length)
+
+end
+
+def swap_elements(a)
+
+  a[0], a[1], a[2] = a[0], a[2], a[1]
+
+end
+
+def reverse_array(a)
+
+a.reverse
+
+end
+
+def kesha_maker(a)
+
+
+
+
+end
